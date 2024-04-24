222aa31976d5721f9bb5fc026f3a4fa4dc751f86
malg10 <malg10@PC85>
2024-04-23 21:04:31 -0300

    Agregar archivos de proyecto.

---
+++ Terciario/Models/Bebida.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terciario.Models
+{
+    class Bebida
+    {
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public Bebida(string nombre, int cantidad)
+        {
+            this.nombre = nombre;
+            this.cantidad = cantidad;
+        }
+        public void tomarse(int cuantoBebio)
+        {
+            this.cantidad -= cuantoBebio;
+        }
+    }
+}
---
+++ Terciario/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terciario.Models;
+
+namespace Terciario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bebida bebida = new Bebida("coquita", 500);
+            Bebida bebida2 = new Bebida("fanta", 600);
+
+            bebida.tomarse(100);
+            bebida2.tomarse(200);
+            Console.WriteLine("Le quedan " + bebida.cantidad + " a la bebida: " + bebida.nombre);
+            Console.WriteLine(bebida2.cantidad);
+        }
+    }
+}
